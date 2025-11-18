571dbeb85d47a63b61b9665db7f87c1dc3877d0f
Matheus Freires <matheusfreires717@gmail.com>
2025-11-17 23:06:23 -0300

    feat: adicionando a main

--- includes/editar.php
+++ includes/editar.php
@@ -1,23 +1,36 @@
+
+    <h2>Editar Contato</h2>
+    <form action="../includes/editar.php" method="POST">
+        <label>Nome:</label>
+        <input type="text" name="nome" required><br><br>
+
+        <label>Email:</label>
+        <input type="email" name="email"><br><br>
+
+        <label>Telefone:</label>
+        <input type="text" name="telefone"><br><br>
+
+        <button type="submit">Salvar</button>
+        <a href='../pages/main.php'>Voltar à lista</a>
+    </form>
+
+
 <?php
-// Importa o arquivo de conexão com o banco de dados
-require_once 'db_connect.php';
+require_once '../includes/db_conection.php'
 
 
-
-// Verifica se foi passado um ID pela URL (GET) para editar um contato existente
-if (isset($_GET['id'])) {
-    $id = $_GET['id']; // Pega o ID do contato enviado pela URL
+    $id = $_GET['id']; // Pega o ID 
 
     // Monta a querry
     $sql = "SELECT * FROM contatos WHERE id = $id";
     $result = $connect->query($sql); // Executa a query
     $dados = $result->fetch_assoc(); // Pega o resultado em forma de array 
-}
 
 
 
 
-// Verifica se o formulário foi enviado (POST)
+
+// Verifica se o formulário foi enviado 
 if ($_POST) {
     // Pega os dados enviados pelo formulário
     $id = $_POST['id'];
@@ -30,7 +43,7 @@ if ($_POST) {
             SET nome='$nome', email='$email', telefone='$telefone' 
             WHERE id = $id";
 
-    // Executa a query se tudo tiver ok
+    // Executa a query, se tudo tiver ok
     if ($connect->query($sql) === TRUE) {
         echo "<p>Contato atualizado com sucesso!</p>";
         echo "<a href='listar.php'>Voltar à lista</a>";
